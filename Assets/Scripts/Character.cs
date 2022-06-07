e51f387663a1a8a574356c200658b2ec31596125
Мусин Михаил <musin.mihail@gmail.com>
2022-06-07 19:05:55 +0300

    движение в центр

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,14 +7,23 @@ public class Character : MonoBehaviour
 {
     SkeletonAnimation skeletonAnimation;
     Spine.Bone bone;
+    [SerializeField] Transform center;
     void Start()
     {
         skeletonAnimation = GetComponent<SkeletonAnimation>();
-        bone = skeletonAnimation.Skeleton.FindBone("crosshair");
-        skeletonAnimation.state.SetAnimation(0, "aim", true);
+        StartCoroutine(MoveCenter());
     }
 
     void Update()
+    {
+
+    }
+    void AimStart()
+    {
+        bone = skeletonAnimation.Skeleton.FindBone("crosshair");
+        skeletonAnimation.state.SetAnimation(0, "aim", true);
+    }
+    void AimUpdate()
     {
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         bone.SetLocalPosition(mouse - transform.position);
@@ -22,5 +31,20 @@ public class Character : MonoBehaviour
         {
             skeletonAnimation.state.SetAnimation(1, "shoot", false);
         }
+    }
+    IEnumerator MoveCenter()
+    {
+        yield return new WaitForSeconds(1);
+        skeletonAnimation.state.SetAnimation(0, "walk", true);
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, center.position, 10 * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+            if(transform.position == center.position)
+            {
+                break;
+            }
+        }
+        skeletonAnimation.state.SetAnimation(0, "idle", true);
     }
 }
